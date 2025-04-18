d4418f0f015d68d9a9c7b1980004a21376933ce2
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-18 17:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,28 +11,69 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+function loadCart() {
+  const cartData = sessionStorage.getItem('cart');
+  if (cartData) {
+    return JSON.parse(cartData);
+  }
+  return [];
+}
+
+function saveCart(cart) {
+  sessionStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
     li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+		const button = li.querySelector('button');
+	    button.addEventListener('click', () => {
+	    const cart = loadCart();
+	    cart.push(product);
+	      saveCart(cart);
+	      renderCart(cart);
+    });
     productList.appendChild(li);
   });
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart(cart) {
+	cartList.innerHTML = '';
+  cart.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${item.name} - $${item.price}`;
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	const product = products.find(p => p.id === productId);
+  if (product) {
+    const cart = loadCart();
+    cart.push(product);
+    saveCart(cart);
+    renderCart(cart);
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+// function removeFromCart(productId) {
+	
+// }
 
 // Clear cart
-function clearCart() {}
-
+function clearCart() {
+	sessionStorage.removeItem('cart');
+  renderCart([]);
+}
+clearCartBtn.addEventListener('click', clearCart);
 // Initial render
 renderProducts();
 renderCart();
